dc042d981ec21f2d79c16d140d050373e2cef6bb
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-07-24 22:00:45 -0500

    Cerrar detalle al abrir carrito de compras

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 //! MENU DESPLEGABLE EMAIL
 
 const menuEmail = document.querySelector(".navbar-email"); //Seleccionar elemento al que se le agregara la interaccion. Elemento con el correo.
-
 const desktopMenu = document.querySelector(".desktop-menu"); //Elemento que se agregara al momento de realizar el click.
 
 menuEmail.addEventListener("click", toggleDesktopMenu); //Cuando se realice click muestre el menu desplegable del email.
 
+let isDetailCartclosed = true;
 function toggleDesktopMenu() {
   //Esta funcion agrega o quita la clase inactive en el elemento desktopMenu, cuando se realice el evento.
   if (!isDetailCartclosed) {
     detailCart.classList.add("inactive");
   }
-
   desktopMenu.classList.toggle("inactive");
 }
 
@@ -33,8 +32,8 @@ function toggleMenuMobile() {
 
 //! MENU CARRITO DE COMPRAS
 
+const productDetailContainer = document.querySelector('#productDetail');
 const menuCartIcon = document.querySelector(".navbar-shopping-cart"); //Selecciono el icono del carrito de compras
-
 const detailCart = document.querySelector(".cart-detail"); //Selecciono el elemento a mostrar, que es el contenedor de los elementos en el carrito de compras.
 
 menuCartIcon.addEventListener("click", toggleCartDetail);
@@ -48,6 +47,14 @@ function toggleCartDetail() {
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add("inactive");
   }
+
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
+  if(!isProductDetailClosed) {
+   productDetailContainer.classList.add('inactive');
+
+}
+
   detailCart.classList.toggle("inactive");
 }
 
@@ -131,7 +138,6 @@ viewDetail.forEach( productCard => {
 //! FUNCION PARA ELIMINAR DETALLE DE UN PRODUCTO
 
 const closeDetail = document.querySelector(".product-detail-close");
-
 closeDetail.addEventListener("click", closeDetailProduct);
 
 function closeDetailProduct() {
